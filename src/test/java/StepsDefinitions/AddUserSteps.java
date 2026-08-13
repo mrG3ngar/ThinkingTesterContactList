5405bff0e526598d75fdfb8af152bf7e7b38163f
mrG3ngar <dlolong11@gmail.com>
2026-08-13 08:09:26 +0800

    Update on practice test automation - code

--- src/test/java/StepsDefinitions/AddUserSteps.java
+++ src/test/java/StepsDefinitions/AddUserSteps.java
@@ -4,7 +4,6 @@ import io.cucumber.java.en.*;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
